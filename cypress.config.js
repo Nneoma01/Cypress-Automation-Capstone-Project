aaa861058eb67c9369fb8012d0ca53656729cc0f
Nneoma01 <toughspirit>
2026-01-14 21:26:01 +0100

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("cypress");
-// const { allureCypress } = require("allure-cypress/reporter");
+
 
 module.exports = defineConfig({
   defaultCommandTimeout: 60000,
@@ -9,14 +9,7 @@ module.exports = defineConfig({
     baseURL: "https://shop.qaautomationlabs.com/index.php",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      //  allureCypress(on, config, {
-      //   resultsDir: "allure-results",
-      // });
-      // require("allure-cypress/reporter")(on);
       return config;
     },
   },
-
-   // specPattern: "cypress/e2e/*.cy.js",
-  retries: 1,
 });
