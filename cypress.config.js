9d5e7e28e465a24f798e71d030f0bcb1ae3ae7fc
Nneoma01 <toughspirit>
2026-01-14 15:36:06 +0100

    add ci.yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,21 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 60000,
   pageLoadTimeout: 60000,
+  watchForFileChanges: false,
   e2e: {
-    watchForFileChanges: false,
+    baseURL: "https://shop.qaautomationlabs.com/index.php",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+       allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 
    // specPattern: "cypress/e2e/*.cy.js",
   retries: 1,
-  env: {
-    baseURL: "https://shop.qaautomationlabs.com/index.php",
-  }
 });
